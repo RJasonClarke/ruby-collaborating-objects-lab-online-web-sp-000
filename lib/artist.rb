33d1e9f9d9073b7a26246f36d7494fbd0eaffe09
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-08 07:04:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,41 +5,39 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
     save
+    @songs = []
   end
 
   def self.all
     @@all
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) || self.create(name)
+  def save
+    @@all << self
+    self
   end
 
   def add_song(song)
-    @songs << song
+    @songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist == self
+
+  end
+
+    def self.find_or_create_by_name(name)
+      self.all.detect{|artist| artist.name == name} || self.new(name)
+
+  end
+
+  def self.find(name)
+    self.all.detect{|artist| artist.name == name}
+  end
+
+  def self.create(name)
+    self.new(name)
   end
 
   def print_songs
-    songs.each { |song| puts song.name }
-  end
-
-  def save
-    @@all << self
-  end
-
-  def songs
-    @songs
+    songs.each{|song| puts song.name}
   end
 end
